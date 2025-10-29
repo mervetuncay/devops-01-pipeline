22a6bd3688461fdc19fb96c09da27142f0135517
mervetuncay <mervetuncay93@hotmail.com>
2025-10-29 19:06:38 +0300

    Devops Pipeline
    Dv Pipeline

---
+++ src/test/java/com/mervetuncay/devops_01_pipeline/Devops01PipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mervetuncay.devops_01_pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devops01PipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
